3dffb463a76dabe067f261534d9572d5b8ec2f49
kumargaurav <kumargaurav@kumargauravs-MacBook-Air.local>
2025-08-18 13:41:30 +0100

    added force destroy to s3

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -5,6 +5,7 @@ resource "aws_s3_bucket" "log_bucket" {
     Name      = "Observability Log Bucket"
     ManagedBy = "Terraform"
   }
+  force_destroy = true
 }
 
 resource "aws_s3_bucket_versioning" "this" {
